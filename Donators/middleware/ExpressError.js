b8e40ea8db6588b2101711297e21d4400e48ca3f
Legacy50633 <112874680+Legacy50633@users.noreply.github.com>
2023-02-09 08:43:09 +0530

    done

--- Donators/middleware/ExpressError.js
+++ Donators/middleware/ExpressError.js
@@ -1,8 +1,8 @@
 class ExpressError extends Error{
     constructor(message,statusCode){
         super()
-        this.message = message
-        this.statusCode = statusCode
+        this.message = message                                  // the class is dedicated to the errors. So we can use this to throw errors when ever we need
+        this.statusCode = statusCode                  
     }
 }
 
