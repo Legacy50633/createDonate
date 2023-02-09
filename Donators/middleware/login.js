66359ff80b06605847947b511f5985686e57b5c6
Legacy50633 <112874680+Legacy50633@users.noreply.github.com>
2023-02-09 08:43:55 +0530

    done

--- Donators/middleware/login.js
+++ Donators/middleware/login.js
@@ -1,6 +1,6 @@
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
-      return  res.redirect('/api/login')
-    }
+      return  res.redirect('/api/login')                                 // this middle ensures the user is authenticated or not
+    }                                                                      
     next()
 }
