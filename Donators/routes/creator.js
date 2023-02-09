a81c0a7aa00c6c4b40eb62a24e9c6e38c457eb67
Legacy50633 <112874680+Legacy50633@users.noreply.github.com>
2023-02-09 08:36:04 +0530

    final touch

--- Donators/routes/creator.js
+++ Donators/routes/creator.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const Creator = require('../Models/creators')
-const {isLoggedIn} = require('../middleware/login')
-const catchAsyncError = require('../middleware/catchAsync')
+const {isLoggedIn} = require('../middleware/login')             //this middleware ensure's whether the user is authenticated or not
+const catchAsyncError = require('../middleware/catchAsync')         // this one will catch asynchronous errors
 
 
 router.get('/api/creators',isLoggedIn,async(req,res)=>{
-const creators = await Creator.find({})
+const creators = await Creator.find({})                                     //paginated api to return the all creators details
 res.render('Creators/creator',{creators})
 })
 
