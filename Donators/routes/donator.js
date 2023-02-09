73d0204659e9da3df08848ad94a66387de04b95a
Legacy50633 <112874680+Legacy50633@users.noreply.github.com>
2023-02-09 08:41:14 +0530

    final touch

--- Donators/routes/donator.js
+++ Donators/routes/donator.js
@@ -5,16 +5,16 @@ const {isLoggedIn} = require('../middleware/login')
 const catchAsyncError = require('../middleware/catchAsync')
 
 router.get('/api/donors',async(req,res)=>{
-    const donors = await Donator.find({})
+    const donors = await Donator.find({})         // api to get all paginated api's
     res.render('Donations/alldonors',{donors})
 })
 
 router.get('/api/donate',isLoggedIn,(req,res)=>{
-    res.render('Donations/donate')
+    res.render('Donations/donate')                               
 })
 
 
-router.get('/api/history',isLoggedIn,(req,res)=>{
+router.get('/api/history',isLoggedIn,(req,res)=>{      //isLoggedIn middleware ensures user logged in or not 
     res.render('Donations/history')
 })
 
@@ -22,13 +22,13 @@ router.get('/api/history',isLoggedIn,(req,res)=>{
 
 router.post('/api/donate',async(req,res)=>{
     const donate =  new Donator(req.body.donate)
-    await donate.save()
+    await donate.save()                                   //this will store every donates from a particular donator to creator
     res.redirect('/api/donors')
     })
 
 
 router.post('/api/history',async(req,res)=>{
-    const {donatorName,id } = req.body
+    const {donatorName,id } = req.body                        // this will retrive the particular donation ftom a donator to receiver
     const donatorHistory = await Donator.find(
         {donatorName:donatorName,id:id})
     res.render("Donations/transfers",{donatorHistory})
