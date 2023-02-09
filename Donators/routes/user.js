90df45ef56c4a993c78bfc21aa1ce9b9628e1b71
Legacy50633 <112874680+Legacy50633@users.noreply.github.com>
2023-02-09 08:33:29 +0530

    Added some  comments

--- Donators/routes/user.js
+++ Donators/routes/user.js
@@ -3,29 +3,29 @@ const passport = require('passport')
 const User = require("../Models/user")
 const router = express.Router()
 
-const catchAsyncError = require('../middleware/catchAsync')
+const catchAsyncError = require('../middleware/catchAsync')        
 
 router.get('/api/register',(req,res)=>{
-    res.render('Authenticate/register')
+    res.render('Authenticate/register')       
 })
 
 
 
 router.get('/api/login',(req,res)=>{
-    res.render('Authenticate/login')
+    res.render('Authenticate/login')         
 })
 
 
-router.post('/api/register',catchAsyncError(async (req,res)=>{
+router.post('/api/register',async (req,res)=>{
     const {email,password,username} = req.body
-    const user = new User({email,username})
+    const user = new User({email,username})                               //the api to register user
     const registeredUser = await User.register(user,password)
     res.redirect('/api/login')
-}))
+})
 
 router.post("/api/login",passport.authenticate('local',
-{failureFlash:false,failureRedirect:"/api/register"}),(req,res)=>{
-res.redirect('/api/home')
+{failureFlash:false,failureRedirect:"/api/register"}),(req,res)=>{            //the api to login user and failure case it will redirect to the register api
+res.redirect('/api/home')                                                  
 
 })
 
